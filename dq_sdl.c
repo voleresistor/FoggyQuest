430773df93512a60319650827d8b59e590fb5d4e
ogden.andrew@gmail.com <ogden.andrew@gmail.com>
2020-09-14 02:20:36 -0500

    Added gamepad support. Most keyboard infrastructure needs to go in the dumper.

--- dq_sdl.c
+++ dq_sdl.c
@@ -5,13 +5,14 @@
 int init_sdl()
 {
     // Set vars to something
-    gRenderer   = NULL;
-    gWindow     = NULL;
-    gFont       = NULL;
-    gTextFont   = NULL;
+    gRenderer           = NULL;
+    gWindow             = NULL;
+    gFont               = NULL;
+    gTextFont           = NULL;
+    gGameController     = NULL;
 
     // Initialize SDL
-    if(SDL_Init(SDL_INIT_VIDEO) < 0)
+    if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK) < 0)
     {
         printf("Couldn't initialize SDL! Error: %s\n", SDL_GetError());
         return EXIT_FAILURE;
@@ -67,6 +68,21 @@ int init_sdl()
     if(Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 2, 4096) < 0 )
     {
         return false;
+    }
+
+    /* init joystick */
+    if(SDL_NumJoysticks() < 1)
+    {
+        printf("Warning: no joysticks detected.\n");
+    }
+    else
+    {
+        /* Load joystick */
+        gGameController = SDL_JoystickOpen(0);
+        if(gGameController == NULL)
+        {
+            printf("Warning: Unable to open game controller! SDL Error: %S\n", SDL_GetError());
+        }
     }
 
     return EXIT_SUCCESS;
@@ -126,6 +142,10 @@ void close_sdl()
     SDL_DestroyWindow(gWindow);
     gRenderer = NULL;
     gWindow = NULL;
+
+    /* Free the joystick */
+    SDL_JoystickClose(gGameController);
+    gGameController = NULL;
 
     /* Free up music file */
     //Mix_FreeMusic(music);
@@ -243,7 +263,7 @@ void video_draw_text(char message[15], int x, int y)
 void controls_handle_input(void)
 {
     /* Buffer to slow down keypresses? */
-    if(key_wait_buffer < 6)
+    if(key_wait_buffer < 5)
     {
         key_wait_buffer++;
         return;
@@ -257,23 +277,85 @@ void controls_handle_input(void)
         /* Click X on the window */
         if(event.type == SDL_QUIT)
         {
+            // This should probably confirm that the user wants to exit
             quit = true;
         }
     }
 
-    if(event.key.type == SDL_TEXTINPUT)
+    /* Reset the key wait buffer if we got valid input */
+    if(event.type == SDL_TEXTINPUT ||
+       event.type == SDL_JOYAXISMOTION ||
+       event.type == SDL_JOYBUTTONDOWN)
     {
-        // printf("SDL_TEXTINPUT\n");
         key_wait_buffer = 0;
     }
 
-    /* Handle mouse and keyboard input */
+    /* Joystick/Gamepad buttons */
+    if(event.type == SDL_JOYBUTTONDOWN)
+    {
+        /* Buttons on controller 0 */
+        if(event.jbutton.which == 0)
+        {
+            /*
+                Buffalo SNES pad mapping:
+                    A           = 0
+                    B           = 1
+                    X           = 2
+                    Y           = 3
+                    L Shoulder  = 4
+                    R Shoulder  = 5
+                    Select      = 6
+                    Start       = 7
+            */
+            // printf("Gamepad Button: %d\n", event.jbutton.button);
+            if(event.jbutton.button == 0) gamepad_b0();
+            if(event.jbutton.button == 1) gamepad_b1();
+            if(event.jbutton.button == 2) gamepad_b2();
+            if(event.jbutton.button == 3) gamepad_b3();
+            if(event.jbutton.button == 4) gamepad_b4();
+            if(event.jbutton.button == 5) gamepad_b5();
+            if(event.jbutton.button == 6) gamepad_b6();
+            if(event.jbutton.button == 7) gamepad_b7();
+        }
+    }
+
+    /* Joystick/Gamepad x/y axes */
+    if(event.type == SDL_JOYAXISMOTION)
+    {
+        /* Motion on controller 0 */
+        if(event.jaxis.which == 0)
+        {
+            /* x axis */
+            if(event.jaxis.axis == 0)
+            {
+                if(event.jaxis.value < 0) gamepad_left();
+                if(event.jaxis.value > 0) gamepad_right();
+            }
+
+            /* y axis */
+            if(event.jaxis.axis == 1)
+            {
+                if(event.jaxis.value < 0) gamepad_up();
+                if(event.jaxis.value > 0) gamepad_down();
+            }
+        }
+    }
+
+    /* Get mouse and keyboard state */
     SDL_GetMouseState(&m_x, &m_y);
     keystates = SDL_GetKeyboardState(NULL);
     //printf("MouseX: %d\tMouseY: %d\n\n", m_x, m_y);
 
-    // if(event.cbutton.state)
+    /*
+        Even though we'd prefer gamepad as a primary, we still
+        have these keyboard keys to fall back on. These might
+        also be useful for grabbing characters straight from
+        the keyboard for things like naming.
 
+        TODO: Add non-alphas and function keys?
+    */
+
+    /* Alpha-numerics */
     if(keystates[SDL_SCANCODE_A] == 1) key_press_a();
     if(keystates[SDL_SCANCODE_B] == 1) key_press_b();
     if(keystates[SDL_SCANCODE_C] == 1) key_press_c();
@@ -310,4 +392,78 @@ void controls_handle_input(void)
     if(keystates[SDL_SCANCODE_7] == 1) key_press_7();
     if(keystates[SDL_SCANCODE_8] == 1) key_press_8();
     if(keystates[SDL_SCANCODE_9] == 1) key_press_9();
+
+    /* Punctuation/Operation */
+    if(keystates[SDL_SCANCODE_APOSTROPHE] == 1)     key_press_apostrophe();
+    if(keystates[SDL_SCANCODE_BACKSLASH] == 1)      key_press_backslash();
+    if(keystates[SDL_SCANCODE_COMMA] == 1)          key_press_comma();
+    if(keystates[SDL_SCANCODE_EQUALS] == 1)         key_press_equals();
+    if(keystates[SDL_SCANCODE_GRAVE] == 1)          key_press_grave();
+    if(keystates[SDL_SCANCODE_PERIOD] == 1)         key_press_period();
+    if(keystates[SDL_SCANCODE_MINUS] == 1)          key_press_minus();
+    if(keystates[SDL_SCANCODE_LEFTBRACKET] == 1)    key_press_lbracket();
+    if(keystates[SDL_SCANCODE_RIGHTBRACKET] == 1)   key_press_rightbracket();
+    if(keystates[SDL_SCANCODE_SEMICOLON] == 1)      key_press_semicolon();
+    if(keystates[SDL_SCANCODE_SLASH] == 1)          key_press_slash();
+    if(keystates[SDL_SCANCODE_SPACE] == 1)          key_press_space();
+
+    /* Keypad */
+    if(keystates[SDL_SCANCODE_KP_0] == 1)           key_press_kp_0();
+    if(keystates[SDL_SCANCODE_KP_1] == 1)           key_press_kp_1();
+    if(keystates[SDL_SCANCODE_KP_2] == 1)           key_press_kp_2();
+    if(keystates[SDL_SCANCODE_KP_3] == 1)           key_press_kp_3();
+    if(keystates[SDL_SCANCODE_KP_4] == 1)           key_press_kp_4();
+    if(keystates[SDL_SCANCODE_KP_5] == 1)           key_press_kp_5();
+    if(keystates[SDL_SCANCODE_KP_6] == 1)           key_press_kp_6();
+    if(keystates[SDL_SCANCODE_KP_7] == 1)           key_press_kp_7();
+    if(keystates[SDL_SCANCODE_KP_8] == 1)           key_press_kp_8();
+    if(keystates[SDL_SCANCODE_KP_9] == 1)           key_press_kp_9();
+    if(keystates[SDL_SCANCODE_KP_DIVIDE] == 1)      key_press_kp_divide();
+    if(keystates[SDL_SCANCODE_KP_MINUS] == 1)       key_press_kp_minus();
+    if(keystates[SDL_SCANCODE_KP_MULTIPLY] == 1)    key_press_kp_multiply();
+    if(keystates[SDL_SCANCODE_KP_PLUS] == 1)        key_press_kp_plus();
+    if(keystates[SDL_SCANCODE_KP_ENTER] == 1)       key_press_kp_enter();
+    if(keystates[SDL_SCANCODE_KP_DECIMAL] == 1)     key_press_kp_decimal();
+
+    /* Function keys */
+    if(keystates[SDL_SCANCODE_F1] == 1)     key_press_f1();
+    if(keystates[SDL_SCANCODE_F2] == 1)     key_press_f2();
+    if(keystates[SDL_SCANCODE_F3] == 1)     key_press_f3();
+    if(keystates[SDL_SCANCODE_F4] == 1)     key_press_f4();
+    if(keystates[SDL_SCANCODE_F5] == 1)     key_press_f5();
+    if(keystates[SDL_SCANCODE_F6] == 1)     key_press_f6();
+    if(keystates[SDL_SCANCODE_F7] == 1)     key_press_f7();
+    if(keystates[SDL_SCANCODE_F8] == 1)     key_press_f8();
+    if(keystates[SDL_SCANCODE_F9] == 1)     key_press_f9();
+    if(keystates[SDL_SCANCODE_F10] == 1)    key_press_f10();
+    if(keystates[SDL_SCANCODE_F11] == 1)    key_press_f11();
+    if(keystates[SDL_SCANCODE_F12] == 1)    key_press_f12();
+
+    /* Other */
+    if(keystates[SDL_SCANCODE_DOWN] == 1)           key_press_down();
+    if(keystates[SDL_SCANCODE_UP] == 1)             key_press_up();
+    if(keystates[SDL_SCANCODE_LEFT] == 1)           key_press_left();
+    if(keystates[SDL_SCANCODE_RIGHT] == 1)          key_press_right();
+    if(keystates[SDL_SCANCODE_APPLICATION] == 1)    key_press_application();
+    if(keystates[SDL_SCANCODE_BACKSPACE] == 1)      key_press_backspace();
+    if(keystates[SDL_SCANCODE_CAPSLOCK] == 1)       key_press_capslock();
+    if(keystates[SDL_SCANCODE_END] == 1)            key_press_end();
+    if(keystates[SDL_SCANCODE_ESCAPE] == 1)         key_press_escape();
+    if(keystates[SDL_SCANCODE_HOME] == 1)           key_press_home();
+    if(keystates[SDL_SCANCODE_INSERT] == 1)         key_press_insert();
+    if(keystates[SDL_SCANCODE_LALT] == 1)           key_press_lalt();
+    if(keystates[SDL_SCANCODE_LCTRL] == 1)          key_press_lctrl();
+    if(keystates[SDL_SCANCODE_LSHIFT] == 1)         key_press_lshift();
+    if(keystates[SDL_SCANCODE_NUMLOCKCLEAR] == 1)   key_press_numlock();
+    if(keystates[SDL_SCANCODE_PAGEDOWN] == 1)       key_press_pgdown();
+    if(keystates[SDL_SCANCODE_PAGEUP] == 1)         key_press_pgup();
+    if(keystates[SDL_SCANCODE_PAUSE] == 1)          key_press_pause();
+    if(keystates[SDL_SCANCODE_PRINTSCREEN] == 1)    key_press_prtscrn();
+    if(keystates[SDL_SCANCODE_RALT] == 1)           key_press_ralt();
+    if(keystates[SDL_SCANCODE_RCTRL] == 1)          key_press_rctrl();
+    if(keystates[SDL_SCANCODE_RETURN] == 1)         key_press_return();
+    if(keystates[SDL_SCANCODE_RETURN2] == 1)        key_press_return2();
+    if(keystates[SDL_SCANCODE_RSHIFT] == 1)         key_press_rshift();
+    if(keystates[SDL_SCANCODE_SCROLLLOCK] == 1)     key_press_scrllck();    
+    if(keystates[SDL_SCANCODE_TAB] == 1)            key_press_tab();
 }
--- dq_sdl.h
+++ dq_sdl.h
@@ -16,6 +16,7 @@ SDL_Renderer*   gRenderer;
 /* Specific SDL assest */
 SDL_Surface*    intro_image;
 SDL_Texture*    intro_texture;
+SDL_Joystick*   gGameController;
 TTF_Font*       gFont;
 SDL_Event       event;
 TTF_Font*       gTextFont;
@@ -24,7 +25,7 @@ int             m_x;
 int             m_y;
 int             hero_move;
 
-/* Function pointers */
+/* Keyboard alpha-numeric function pointers */
 void (*key_press_a)(void);
 void (*key_press_b)(void);
 void (*key_press_c)(void);
@@ -61,6 +62,94 @@ void (*key_press_6)(void);
 void (*key_press_7)(void);
 void (*key_press_8)(void);
 void (*key_press_9)(void);
+
+/* Keyboard punctuation/operator pointers */
+void (*key_press_apostrophe)(void);
+void (*key_press_backslash)(void);
+void (*key_press_comma)(void);
+void (*key_press_equals)(void);
+void (*key_press_grave)(void);
+void (*key_press_period)(void);
+void (*key_press_minus)(void);
+void (*key_press_lbracket)(void);
+void (*key_press_rightbracket)(void);
+void (*key_press_semicolon)(void);
+void (*key_press_slash)(void);
+void (*key_press_space)(void);
+
+/* Keyboard keypad function pointers */
+void (*key_press_kp_0)(void);
+void (*key_press_kp_1)(void);
+void (*key_press_kp_2)(void);
+void (*key_press_kp_3)(void);
+void (*key_press_kp_4)(void);
+void (*key_press_kp_5)(void);
+void (*key_press_kp_6)(void);
+void (*key_press_kp_7)(void);
+void (*key_press_kp_8)(void);
+void (*key_press_kp_9)(void);
+void (*key_press_kp_divide)(void);
+void (*key_press_kp_minus)(void);
+void (*key_press_kp_multiply)(void);
+void (*key_press_kp_plus)(void);
+void (*key_press_kp_enter)(void);
+void (*key_press_kp_decimal)(void);
+
+/* Keyboard function keys */
+void (*key_press_f1)(void);
+void (*key_press_f2)(void);
+void (*key_press_f3)(void);
+void (*key_press_f4)(void);
+void (*key_press_f5)(void);
+void (*key_press_f6)(void);
+void (*key_press_f7)(void);
+void (*key_press_f8)(void);
+void (*key_press_f9)(void);
+void (*key_press_f10)(void);
+void (*key_press_f11)(void);
+void (*key_press_f12)(void);
+
+/* Keyboard other keys */
+void (*key_press_down)(void);
+void (*key_press_up)(void);
+void (*key_press_left)(void);
+void (*key_press_right)(void);
+void (*key_press_application)(void);
+void (*key_press_backspace)(void);
+void (*key_press_capslock)(void);
+void (*key_press_end)(void);
+void (*key_press_escape)(void);
+void (*key_press_home)(void);
+void (*key_press_insert)(void);
+void (*key_press_lalt)(void);
+void (*key_press_lctrl)(void);
+void (*key_press_lshift)(void);
+void (*key_press_numlock)(void);
+void (*key_press_pgdown)(void);
+void (*key_press_pgup)(void);
+void (*key_press_pause)(void);
+void (*key_press_prtscrn)(void);
+void (*key_press_ralt)(void);
+void (*key_press_rctrl)(void);
+void (*key_press_return)(void);
+void (*key_press_return2)(void);
+void (*key_press_rshift)(void);
+void (*key_press_scrllck)(void);
+void (*key_press_tab)(void);
+
+/* Gamepad function pointers */
+void (*gamepad_up)(void);
+void (*gamepad_down)(void);
+void (*gamepad_left)(void);
+void (*gamepad_right)(void);
+void (*gamepad_b0)(void);
+void (*gamepad_b1)(void);
+void (*gamepad_b2)(void);
+void (*gamepad_b3)(void);
+void (*gamepad_b4)(void);
+void (*gamepad_b5)(void);
+void (*gamepad_b6)(void);
+void (*gamepad_b7)(void);
 
 /* Area render control */
 int tile_size;
